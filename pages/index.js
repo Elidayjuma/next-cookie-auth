2948a8e0f64e14d058406216991ff6f9a8e9e9ad
unknown <elidejuma@gmail.com>
2019-09-25 18:52:34 +0300

    This commit is the initial comit though it is not stable. The login button is not functioning.

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+import { authInitialProps } from '../lib/auth';
+
+export default function Index(props) {
+   // console.log(...this.props);
+    return (
+        <Layout title="Home" {...props}>
+            <Link href="/profile"><a>Go to Profile</a></Link>
+        </Layout>
+    )
+}
+
+Index.getInitialProps = authInitialProps();
